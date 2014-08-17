c9bbb36a0ea80e86680845c15bdafff9bcc4fa25
betahikaru <beta.hikaru2000+github@gmail.com>
2014-08-17 22:50:14 +0900

    Add package 'auto-complete'

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,6 +2,10 @@
 (require 'cask "~/.emacs.d/.cask/cask.el")
 (cask-initialize)
 
+;; auto-complete
+(autoload 'auto-complete-mode "auto-complete.el" "auto-complete" t)
+(ac-config-default)
+
 ;; markdown-mode
 (autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
