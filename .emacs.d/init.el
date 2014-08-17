e4bee82a8e62ddf38486719e2c7ac073eebf1c3a
betahikaru <beta.hikaru2000+github@gmail.com>
2014-08-17 23:35:22 +0900

    Add package 'helm' (like anything)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,6 +6,9 @@
 (autoload 'auto-complete-mode "auto-complete.el" "auto-complete" t)
 (ac-config-default)
 
+;; helm
+(global-set-key (kbd "C-c h") 'helm-mini)
+
 ;; markdown-mode
 (autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
