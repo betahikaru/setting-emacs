5e259d508101015bfb86f435e0c3f447ab57a1ca
betahikaru <beta.hikaru2000+github@gmail.com>
2014-08-20 23:55:01 +0900

    Add package 'nlinum' (show line number)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -27,6 +27,13 @@
 ;; ace-jump-mode
 (define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
 
+;; nlinum
+;;; Default is hidden line-number.
+;;; Press F6 key, toggle line-number.
+(global-linum-mode 1)
+(global-set-key [f6] 'linum-mode)
+(setq linum-format "%4d ")
+
 ;; markdown-mode
 (autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
